e236144989bdc8a50db1637d8e7b9fc3d37fb28b
k-metra <metrakurt@gmail.com>
2025-09-21 10:12:11 +0800

    add register API

--- loginform/accounts/views.py
+++ loginform/accounts/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages as msgs
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
+from django.contrib.auth.models import User
 
 def login_view(request):
     if request.method == "POST":
@@ -39,3 +40,32 @@ def login_api(request):
             return JsonResponse({"success": False, "error": "Invalid username or password."}, status=401)
     
     return JsonResponse({"error": "Only POST allowed"}, status=405)
+
+def register_page(request):
+    if request.user.is_authenticated:
+        msgs.error(request, "You are already logged in!")
+        
+        return JsonResponse({"error": "User already authenticated."}, status=421)
+
+    return render(request, "accounts/register.html")    
+
+def register_api(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({"success": False, "error": "Username already taken."}, status=400)
+        
+        try:
+            user = User.objects.create_user(
+                username=username,
+                email="",
+                password=password
+            )
+
+            return JsonResponse({"success": True, "message": "User registered successfully."})
+        except Exception as e:
+            return JsonResponse({"success": False, "error": str(e)}, status=500)
+    
+    return JsonResponse({"success": False, "error":"Invalid request"})
