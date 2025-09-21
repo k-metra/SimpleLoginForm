e3c0150158e41aa6642cd0d8ef94dbc2e0c0d74b
k-metra <metrakurt@gmail.com>
2025-09-21 10:12:02 +0800

    add register links

--- loginform/accounts/urls.py
+++ loginform/accounts/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path("logout/", view=views.logout_view, name="logout"),
     path("dashboard/", views.dashboard, name="dashboard"),
     path("login-api/", views.login_api, name="login_api"),
+
+    path("register/", views.register_page, name="register"),
+    path("register-api/", views.register_api, name="register_api"),
+
 ]
